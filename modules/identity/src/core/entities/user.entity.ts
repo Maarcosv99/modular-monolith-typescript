9e4d17c0d4a95a2aa2c3258505e449a567db98fa
davisvkz <davissviana2006@gmail.com>
2025-07-25 21:35:36 -0300

    fix: fix ddd components integrations to application

--- modules/identity/src/core/entities/user.entity.ts
+++ modules/identity/src/core/entities/user.entity.ts
@@ -1,10 +1,11 @@
-import { Entity } from '@modules/shared/core/entity';
+import { Entity } from '@modules/shared/core/Entity';
+import { UniqueEntityID } from '@modules/shared/core/UniqueEntityID';
 
 import { Email } from 'core/value-objects/email.value-object';
 import { Password } from 'core/value-objects/password.value-object';
 
 export interface UserProps {
-  id?: string
+  id?: UniqueEntityID
   first_name: string
   last_name: string
   email: Email
@@ -14,36 +15,36 @@ export interface UserProps {
 export class User extends Entity<UserProps> {
   private constructor(
     props: UserProps,
-    id?: string,
+    id?: UniqueEntityID,
   ) {
     super(props, id);
   }
 
-  get id(): string {
+  get id(): UniqueEntityID {
     return this._id;
   }
 
   get first_name(): string {
-    return this._props.first_name;
+    return this.props.first_name;
   }
 
   get last_name(): string {
-    return this._props.last_name;
+    return this.props.last_name;
   }
 
   get full_name(): string {
-    return `${this._props.first_name} ${this._props.last_name}`;
+    return `${this.props.first_name} ${this.props.last_name}`;
   }
 
   get email(): Email {
-    return this._props.email;
+    return this.props.email;
   }
 
   get password(): Password {
-    return this._props.password;
+    return this.props.password;
   }
 
-  static create(props: UserProps, id?: string): User {
+  static create(props: UserProps, id?: UniqueEntityID): User {
     return new User(props, id);
   }
 }
--- modules/identity/src/core/value-objects/email.value-object.ts
+++ modules/identity/src/core/value-objects/email.value-object.ts
@@ -1,20 +1,27 @@
 import type { Result } from '@modules/shared/core/result';
 import { Success, Failure } from '@modules/shared/core/result';
+import { ValueObject } from '@modules/shared/core/ValueObject';
 
 import { InvalidEmailException } from 'core/exceptions/invalid-email.exception';
 
-export class Email {
-  private constructor(public readonly value: string) {}
+interface EmailProp{
+  value: string
+}
 
-  static validate(value: string): boolean {
-    if (!value.includes('@')) {
+export class Email extends ValueObject<EmailProp>{
+  private constructor(public readonly props: EmailProp) {
+    super(props)
+  }
+
+  static validate(props: EmailProp): boolean {
+    if (!props.value.includes('@')) {
       return false;
     }
 
     return true;
   }
 
-  static create(value: string): Result<Email, InvalidEmailException> {
+  static create(value: EmailProp): Result<Email, InvalidEmailException> {
     if (!Email.validate(value)) {
       return Failure(new InvalidEmailException('Invalid email'));
     }
