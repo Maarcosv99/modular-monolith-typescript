8498146c85d8fa2f6e34228b5bc6af155709289e
Marcos Viana <marcosvianacdc@gmail.com>
2025-07-30 14:30:33 -0300

    feat: create public file to share content for another modules

---
+++ modules/identity/src/public.ts
@@ -0,0 +1,9 @@
+// Export type, interfaces, enums, and client services that are public
+
+import { AuthenticatedMiddleware } from 'http/rest/middlewares/authenticated.middleware';
+
+export default {
+  middlewares: {
+    AuthenticatedMiddleware
+  }
+};
